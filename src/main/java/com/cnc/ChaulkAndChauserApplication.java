70634d5ddc2ade0b957bac6b2e9c1946621601b8
Rusiru Saumya <rsekanayake1994@gmail.com>
2021-01-16 08:55:51 -0800

    First

--- src/main/java/com/cnc/ChaulkAndChauserApplication.java
+++ src/main/java/com/cnc/ChaulkAndChauserApplication.java
@@ -8,7 +8,7 @@ public class ChaulkAndChauserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChaulkAndChauserApplication.class, args);
-		System.out.println("Working");
+		System.out.println("It's Working");
 	}
 
 
